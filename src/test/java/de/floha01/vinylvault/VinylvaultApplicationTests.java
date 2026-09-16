de2bd280cf27af37ef22d566b858b6cfd7494b26
Flo <flo.hartl@gmx.net>
2026-09-16 14:32:39 +0200

    Initial commit

---
+++ src/test/java/de/floha01/vinylvault/VinylvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package de.floha01.vinylvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinylvaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
